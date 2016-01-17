9c79ffc43ed92c62044fba6c1817dbd1f4ca8e1a
KattySun <swkatty@gmail.com>
2016-01-17 21:44:15 +0300

    change page load listener

--- js/script.js
+++ js/script.js
@@ -1,95 +1,97 @@
 'use strict';
 
-window.onload = function() {
-    var result = document.getElementById('result'),
-        message = document.getElementById('message'),
-        reset = document.getElementById('reset'),
-        table = document.getElementById('tableId'),
-        sum = new SumCells();
+function ready() {
+  var result = document.getElementById('result'),
+      message = document.getElementById('message'),
+      reset = document.getElementById('reset'),
+      table = document.getElementById('tableId'),
+      sum = new SumCells();
 
-    function SumCells() {
-      this.sumItems = 0;
-      var that = this;
-      var selectedTd;
+  function SumCells() {
+    this.sumItems = 0;
+    var that = this;
+    var selectedTd;
+    
+    table.onclick = function(event) {
+      var target = event.target; 
       
-      table.onclick = function(event) {
-        var target = event.target; 
-        
-        if (target.tagName != 'TD') return; 
-           
-        hideMessage();
-        setHighlight(target);
-        
-        var value = target.innerHTML;
-        
-        if (isNumeric(value)) { 
-          that.sumItems += +value;
-          that.sumItems = +that.sumItems.toFixed(10);
-          result.innerHTML = that.sumItems;
-        } else {
-          showMessage('Значение не является числовым!');
-        }
-      };
+      if (target.tagName != 'TD') return; 
+         
+      hideMessage();
+      setHighlight(target);
       
-      reset.onclick = function() {
-        hideMessage();             
-        removeHighlight();
-        that.sumItems = 0;
-        result.innerHTML = that.sumItems; 
-       };
+      var value = target.innerHTML;
       
-      /*
-       * Возвращает true, если value число. 
-       * В противном случае возвращает false
-       *
-       * @param value Значение для проверки.
-       */
-      function isNumeric(value) {
-        return !isNaN(parseFloat(value)) && isFinite(value);
+      if (isNumeric(value)) { 
+        that.sumItems += +value;
+        that.sumItems = +that.sumItems.toFixed(10);
+        result.innerHTML = that.sumItems;
+      } else {
+        showMessage('Значение не является числовым!');
       }
-      
-      /*
-       * Скрывает информационное сообщение.
-       * Необходимо для скрытия сообщения о 
-       * не корректных данных для суммирования.
-       */
-      function hideMessage() {
-        if (message.style.display != 'none') {   
-          message.innerHTML = '';        
-          message.style.display = 'none';
-        } 
-      }
+    };
+    
+    reset.onclick = function() {
+      hideMessage();             
+      removeHighlight();
+      that.sumItems = 0;
+      result.innerHTML = that.sumItems; 
+     };
+    
+    /*
+     * Возвращает true, если value число. 
+     * В противном случае возвращает false
+     *
+     * @param value Значение для проверки.
+     */
+    function isNumeric(value) {
+      return !isNaN(parseFloat(value)) && isFinite(value);
+    }
+    
+    /*
+     * Скрывает информационное сообщение.
+     * Необходимо для скрытия сообщения о 
+     * не корректных данных для суммирования.
+     */
+    function hideMessage() {
+      if (message.style.display != 'none') {   
+        message.innerHTML = '';        
+        message.style.display = 'none';
+      } 
+    }
 
-      /*
-       * Отображает информационное сообщение text.
-       *
-       * @param test Текст сообщения.
-       */
-      function showMessage(text) {
-        if (message.style.display != 'block') {   
-          message.innerHTML = text;
-          message.style.display = 'block';
-        }  
-      }
+    /*
+     * Отображает информационное сообщение text.
+     *
+     * @param test Текст сообщения.
+     */
+    function showMessage(text) {
+      if (message.style.display != 'block') {   
+        message.innerHTML = text;
+        message.style.display = 'block';
+      }  
+    }
 
-      /*
-       * Удаление подсветки выьранной ячейки 
-       */
-      function removeHighlight() {
-        if (selectedTd) {
-          selectedTd.classList.remove('highlight');
-        }
-      }
-      
-      /*
-       * Установка подсветки выбранной ячейки node 
-       *
-       * @param node Выбранный элемент TD.
-       */
-      function setHighlight(node) {
-        removeHighlight();
-        selectedTd = node;
-        selectedTd.classList.add('highlight');
+    /*
+     * Удаление подсветки выьранной ячейки 
+     */
+    function removeHighlight() {
+      if (selectedTd) {
+        selectedTd.classList.remove('highlight');
       }
     }
-};
+    
+    /*
+     * Установка подсветки выбранной ячейки node 
+     *
+     * @param node Выбранный элемент TD.
+     */
+    function setHighlight(node) {
+      removeHighlight();
+      selectedTd = node;
+      selectedTd.classList.add('highlight');
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", ready);
